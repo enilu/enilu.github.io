674053a2f4bcc200fcdffbd9296ed426d6c47d87
enilu <82552623@qq.com>
2015-11-25 00:01:18 +0800

    Added 数据抓取之反爬虫规则：验证码识别

--- assets/scripts/blog_by_year.js
+++ assets/scripts/blog_by_year.js
@@ -55,7 +55,7 @@ var year2014 = [{
     , {'title': '', 'href': '', 'descript': ''}
     , {'title': '', 'href': '', 'descript': ''}
     , {'title': '', 'href': '', 'descript': ''}];
-var year2015 = [{
+var year2015 = [{'title':'数据抓取之验证码识别','href':'blog/2015/crawler_ocr.html','descript':'数据抓取过程中，验证码是一个必须面对的坎。总体来说验证码识别分两种，机器识别和人工识别，随着现在验证码越来越变态，要想机器识别验证码已经越来越难了，典型的入12306那种已经更改为图像识别，而不是简单文字识别了。'},{
     'title': '数据抓取之数据抓取流程', 'img': 'blog/2015/images/crawler_process/crawler1.png',
     'href': 'blog/2015/crawler_process.html', 'tag': '数据抓取', 'date': '23/11/2015',
     'descript': '公司的数据抓取系统也写了一阵子了，是时候总结下了，不然凭我的记性，过一段时间就忘的差不多了。打算写一个系列将其中踩过的坑都记录下来。暂时定一个目录，按照这个系列来写'
